4cd23edb6475b668fd42bb3f7455fa6d77f7f2c9
miguel21ati02 <miguel21ati02@gmail.com>
2025-06-02 02:45:33 +0000

    Primeira versão da função checkItem()

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ const items = []
                 <div>
                     <input type="checkbox" name="list" id="item-${index}">
 
-                    <div class="custom-checkbox">
+                    <div class="custom-checkbox" onclick="checkItem('${item.name}')">
                         <img src="./assets/checked.svg" alt="checked">
                     </div>
-                    <label for="item-${index}">${item.name}</label>
+                    <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
                 </div>
 
                 <button onclick="removeItem('${item.name}')">
@@ -51,6 +51,18 @@ function removeItem(itemName) {
 
     if (itemIndex !== -1) {
         items.splice(itemIndex, 1)
+    }
+
+    showItemsList()
+}
+
+function checkItem(itemName) {
+    const item = items.find((item) => item.name === itemName)
+
+    if(item.checked === true) {
+        item.checked = false
+    } else {
+        item.checked = true 
     }
 
     showItemsList()
