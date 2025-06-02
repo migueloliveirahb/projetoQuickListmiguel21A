1d3202a4ee7918894ef76ae0a2dd3915e7fb8964
miguel21ati02 <miguel21ati02@gmail.com>
2025-06-02 02:30:38 +0000

    Início da função removeItem para remover os itens da lista

--- script.js
+++ script.js
@@ -31,10 +31,20 @@ const items = []
                     <label for="item-${index}">${item.name}</label>
                 </div>
 
-                <button>
+                <button onclick="removeItem('${item.name}')">
                     <img src="./assets/trash-icon.svg" alt="trash icon">
                 </button>
             </div>
             `
         })
     }
+
+function removeItem(itemName) {
+    const itemIndex = items.findIndex((item) => item.name === itemName)
+
+    if (itemIndex !== -1) {
+        items.splice(itemIndex, 1)
+    }
+
+    showItemsList()
+}
