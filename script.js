61b8ef009fa4c5ad766903751798df5199763267
miguel21ati02 <miguel21ati02@gmail.com>
2025-06-02 02:08:09 +0000

    Continuação da função de criar novos itens criando o objeto de cada item para ser adicionado ao array de items

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ const items = []
 
     function addItem() {
         const itemName = document.querySelector("#item").value
+
+        const item = {
+            name: itemName,
+            checked: false
+        }
+
+        items.push(item)
     }
